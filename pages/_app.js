5b6fd4c5dac548a14755024a1021c94d40c87b0d
Musasizi <markmusasizi@gmail.com>
2021-11-10 12:39:23 +0300

    Components that use recoil state need RecoilRoot to appear somewhere in the parent tree.

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import '../styles/globals.css'
 import { ChakraProvider } from "@chakra-ui/react"
 import {AuthUserProvider} from '../context/AuthUserContext.js'
+import {RecoilRoot} from 'recoil';
 function MyApp({ Component, pageProps }) {
   
     return (
       <AuthUserProvider>
         <ChakraProvider>
+        <RecoilRoot>
          <Component {...pageProps} />
+         </RecoilRoot>
       </ChakraProvider>
      
       </AuthUserProvider>
